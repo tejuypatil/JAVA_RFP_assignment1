05dd4a195d990bb0820e8fcc53beaa0e88981e9c
tejuypatil <tejuypatil@gmail.com>
2022-07-01 18:25:31 +0530

    Corrected Spelling

--- src/my/practice/EmpWageMain.java
+++ src/my/practice/EmpWageMain.java
@@ -3,6 +3,6 @@ package my.practice;
 public class EmpWageMain {
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Emplyoee Wage Comuptation");
+        System.out.println("Welcome to Employee Wage Computation");
     }
 }
