88bb9f34d58d5e2f9118e8887d59ab1dd1916d24
tejuypatil <tejuypatil@gmail.com>
2022-07-01 19:06:25 +0530

    Added Employee Attendance

--- src/my/practice/EmpWageMain.java
+++ src/my/practice/EmpWageMain.java
@@ -4,5 +4,17 @@ public class EmpWageMain {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage Computation");
+
+        int randomCheck= (int) Math.round(Math.random());
+        if ( 1 == randomCheck)
+        {
+            System.out.println("Employee is present");
+        }
+
+        else
+        {
+            System.out.println("Employee is absent");
+        }
+
     }
 }
